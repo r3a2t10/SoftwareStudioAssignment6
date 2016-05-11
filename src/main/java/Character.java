621c23300d44789b8798eabf07f30f44a7ce7ca6
jocelynzungchen <jocelynzung@gmail.com>
2016-05-11 15:27:46 +0800

    clear to origin
    Signed-off-by: jocelynzungchen <jocelynzung@gmail.com>

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,8 +1,5 @@
 package main.java;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import processing.core.PApplet;
 
 /**
@@ -12,48 +9,15 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
-	private String name, colour;
-	public float x, y, radius;
-	public float initX, initY;  ///
-	public ArrayList<Character> targets;
-	public HashMap<Character, Integer> targets_value;
 
-	public Character(MainApplet parent, String name, String colour, float x,float y){
+	public Character(MainApplet parent){
+
 		this.parent = parent;
-		this.name = name;
-		this.colour = colour;
-		this.x = x;
-		this.y = y;
-		this.initX = x;
-		this.initY = y;
-		//this.charactersOnCircle = charactersOnCircle ;
-		this.radius = 30;
-		// Initialize the target list
-		this.targets = new ArrayList<Character>();
-		this.targets_value = new HashMap<Character, Integer>();
+		
 	}
 
 	public void display(){
-		//this.parent.background(255);
-		//decode string he into RGB integer
-		long thisCol=Long.decode(colour)+4278190080L;
-		int useColour=(int)thisCol;
-		
-		this.parent.fill(useColour);
-		this.parent.noStroke();//no ellipse stroke
-		this.parent.ellipse(this.x, this.y, radius, radius);///
-	}
-	public void showName(){
-		this.parent.fill(51, 51, 153);//fill the name color
-		this.parent.textSize(15);
-		this.parent.text(this.name, this.x, this.y);
-	}
-	public void addTarget(Character target, int value){
-		this.targets.add(target);
-		this.targets_value.put(target, value);
+
 	}
 	
-	public ArrayList<Character> getTargets(){
-		return this.targets;
-	}
 }
